01915ce954c8b1afc3207818efb4219fc94c3735
Armando portatil <betancourt2498@gmail.com>
2026-06-12 11:24:29 -0400

    Configuración de base path para Vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/paginapetrolera-/', // Añadimos esto para que las rutas funcionen en GitHub Pages
 })
